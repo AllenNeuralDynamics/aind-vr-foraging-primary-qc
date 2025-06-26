15ece0485e5b0ea0501278128e821a8e9c2bf81c
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-06-26 13:14:02 -0700

    Add local exporter

---
+++ code/qc_exporter.py
@@ -0,0 +1,105 @@
+import itertools
+import os
+import typing as t
+import uuid
+from datetime import datetime, timezone
+from pathlib import Path
+
+import pydantic
+from aind_data_schema.core.quality_control import (
+    QCEvaluation,
+    QCMetric,
+    QCStatus,
+    QualityControl,
+    Status,
+)
+from aind_data_schema_models.modalities import Modality
+from contraqctor import qc
+from matplotlib.figure import Figure
+
+from aind_behavior_vr_foraging import data_qc
+
+EVALUATOR = "Automated"
+NOW = datetime.now(timezone.utc)
+s = QCStatus(evaluator=EVALUATOR, status=Status.PASS, timestamp=NOW)
+sp = QCStatus(evaluator=EVALUATOR, status=Status.PENDING, timestamp=NOW)
+
+
+status_converter = {
+    qc.Status.PASSED: Status.PASS,
+    qc.Status.SKIPPED: Status.PASS,
+    qc.Status.WARNING: Status.PENDING,
+    qc.Status.FAILED: Status.FAIL,
+    qc.Status.ERROR: Status.FAIL,
+}
+
+
+def result_to_qc_metric(
+    result: qc.Result, create_assets: bool = False, asset_root: os.PathLike = Path(".")
+) -> t.Optional[QCMetric]:
+    status = QCStatus(
+        evaluator=EVALUATOR, status=status_converter[result.status], timestamp=NOW
+    )
+
+    return QCMetric(
+        name=f"{result.suite_name}::{result.test_name}",
+        description=f"Test: {result.description} // Message: {result.message}",
+        value=result.result,
+        status_history=[status],
+        reference=_resolve_reference(result, asset_root) if create_assets else None,
+    )
+
+
+def _resolve_reference(
+    result: qc.Result, asset_root: os.PathLike = Path(".")
+) -> t.Optional[str]:
+    if not isinstance(result.context, dict):
+        return None
+    asset = result.context.get("asset", None)
+    if isinstance(asset, qc._context_extensions.ContextExportableObj):
+        if isinstance(asset.asset, Figure):
+            random_hash = uuid.uuid4().hex
+            path = f"{result.suite_name}_{result.test_name}_{random_hash}.png"
+            asset.asset.savefig(Path(asset_root) / path)
+            return path
+    return None
+
+
+def to_ads(
+    results: t.Dict[str | None, t.List[qc.Result]], cli_args: "QCCli"
+) -> QualityControl:
+    evals = []
+    for group_name, group in results.items():
+        groupby_test_suite = itertools.groupby(group, lambda x: x.suite_name)
+        for suite_name, test_results in groupby_test_suite:
+            if not test_results:
+                continue
+            _test_results = list(test_results)
+            metrics = [
+                result_to_qc_metric(
+                    r, create_assets=True, asset_root=cli_args.asset_path
+                )
+                for r in _test_results
+            ]
+            metrics = [m for m in metrics if m is not None]
+            evals.append(
+                QCEvaluation(
+                    modality=Modality.BEHAVIOR,
+                    stage="Raw data",
+                    name=f"{group_name if group_name else 'NoGroup'}::{suite_name}",
+                    created=NOW,
+                    metrics=metrics,
+                )
+            )
+    return QualityControl(evaluations=evals)
+
+
+class QCCli(data_qc._QCCli):
+    qc_json_path: Path = pydantic.Field(
+        default=Path("qc.json"),
+        description="Path to export the QC results in ADS format. If not provided, results will not be exported.",
+    )
+    asset_path: Path = pydantic.Field(
+        default=Path("."),
+        description="Path to the asset root directory. If not provided, the current working directory will be used.",
+    )
--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,13 +1,17 @@
-from aind_behavior_vr_foraging import data_qc, data_qc_ads_exporter
+from aind_behavior_vr_foraging import data_qc
 from pathlib import Path
+from qc_exporter import to_ads, QCCli
 
-if __name__ == '__main__':
-    path = Path('/root/capsule/data/behavior_789923_2025-05-13_16-16-16')
-    parsed_args = data_qc_ads_exporter._QCCli(data_path=path, qc_json_path=Path('/results'), asset_path=Path('/results'))
+if __name__ == "__main__":
+    path = Path("/root/capsule/data/behavior_789923_2025-05-13_16-16-16")
+    parsed_args = QCCli(
+        data_path=path, qc_json_path=Path("/results"), asset_path=Path("/results")
+    )
     vr_dataset = data_qc.dataset(parsed_args.data_path)
 
     runner = data_qc.make_qc_runner(vr_dataset)
     results = runner.run_all_with_progress()
-    qc_json = data_qc_ads_exporter.to_ads(results, parsed_args)
-    with open('/results/quality_control.json', 'w') as f:
-        f.write(qc_json.model_dump_json(indent=2))
+    qc_json = to_ads(results, parsed_args)
+    if parsed_args.qc_json_path is not None:
+        with open(parsed_args.qc_json_path, "w", encoding="utf-8") as f:
+            f.write(qc_json.model_dump_json(indent=2))
