6144aa28f74b860d1e3cf449abfbfa10b4e104a4
arjunsridhar12345 <arjun.sridhar@alleninstitute.org>
2025-06-26 17:55:09 +0000

    add initial qc

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,13 @@
-""" top level run script """
+from aind_behavior_vr_foraging import data_qc, data_qc_ads_exporter
+from pathlib import Path
 
-def run():
-    """ basic run function """
-    pass
+if __name__ == '__main__':
+    path = Path('/root/capsule/data/behavior_789923_2025-05-13_16-16-16')
+    parsed_args = data_qc_ads_exporter._QCCli(data_path=path, qc_json_path=Path('/results'), asset_path=Path('/results'))
+    vr_dataset = data_qc.dataset(parsed_args.data_path)
 
-if __name__ == "__main__": run()
+    runner = data_qc.make_qc_runner(vr_dataset)
+    results = runner.run_all_with_progress()
+    qc_json = data_qc_ads_exporter.to_ads(results, parsed_args)
+    with open('/results/quality_control.json', 'w') as f:
+        f.write(qc_json.model_dump_json(indent=2))
