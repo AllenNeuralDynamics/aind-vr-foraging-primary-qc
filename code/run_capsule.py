da50652305cfbc286084abc0b7b9d5ae2bc4c13c
arjunsridhar12345 <109561860+arjunsridhar12345@users.noreply.github.com>
2025-08-14 17:03:06 -0700

    Primary data qc (#4)
    * add initial qc
    
    * Use uv to bootstrap dependencies
    
    * Add local exporter
    
    * Fix artifact path
    
    * Retarget vr-foraging branch
    
    * add logging, settings class, and update run shell script
    
    * use generic mount name
    
    * docs: initial readme
    
    ---------
    
    Co-authored-by: bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
    Co-authored-by: bruno-f-cruz <bruno.cruz@alleninstitute.org>

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,50 @@
-""" top level run script """
+import logging
+from aind_behavior_vr_foraging import data_qc
+from pathlib import Path
+from pydantic import Field
+from pydantic_settings import BaseSettings
+from qc_exporter import to_ads, QCCli
 
-def run():
-    """ basic run function """
-    pass
+logger = logging.getLogger(__name__)
 
-if __name__ == "__main__": run()
+class VRForagingSettings(BaseSettings, cli_parse_args=True):
+    """
+    Settings for VR Foraging Primary Data NWB Packaging
+    """
+
+    input_directory: Path = Field(
+        default=Path("/data/"), description="Directory where data is"
+    )
+    output_directory: Path = Field(
+        default=Path("/results/"), description="Output directory"
+    )
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    settings = VRForagingSettings()
+    primary_data_path = tuple(settings.input_directory.glob("*"))
+    if not primary_data_path:
+        raise FileNotFoundError("No primary data asset attached")
+
+    if len(primary_data_path) > 1:
+        raise ValueError(
+            "Multiple primary data assets attached. Only single asset needed"
+        )
+
+    logger.info(f"Running qc on primary data at path {primary_data_path[0]}")
+    parsed_args = QCCli(
+        data_path=primary_data_path[0], qc_json_path=settings.output_directory, asset_path=settings.output_directory
+    )
+    vr_dataset = data_qc.dataset(parsed_args.data_path)
+
+    runner = data_qc.make_qc_runner(vr_dataset)
+    results = runner.run_all_with_progress()
+    qc_json = to_ads(results, parsed_args)
+    if parsed_args.qc_json_path is not None:
+        with open(parsed_args.qc_json_path / "quality_control.json", "w", encoding="utf-8") as f:
+            f.write(qc_json.model_dump_json(indent=2))
+    
+    logger.info(f"Finished qc. Output saved to {settings.output_directory}")
